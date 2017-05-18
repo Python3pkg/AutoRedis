957003fc7b5303d1e6e5994cbeb6cb90deb7ccc8
Python3pkg <raliclo@gmail.com>
2017-05-17 22:46:31 -0700

    Convert to python3

--- examples/autoredis_sentinel.py
+++ examples/autoredis_sentinel.py
@@ -14,11 +14,11 @@ if __name__ == '__main__':
     AUTO_REDIS.sadd('myset', 'mydata')
     for i in range(1, 10):
         try:
-            print(AUTO_REDIS.smembers('myset'))
-            print(AUTO_REDIS.on_master('ping'))
+            print((AUTO_REDIS.smembers('myset')))
+            print((AUTO_REDIS.on_master('ping')))
             if AUTO_REDIS.slaves:
-                print(AUTO_REDIS.on_slave(('127.0.0.1', 6381), 'ping'))
+                print((AUTO_REDIS.on_slave(('127.0.0.1', 6381), 'ping')))
         except RedisConnectionError:
             print('No slave or master up')
             sys.exit(1)
-        print('Slaves: ' + str(AUTO_REDIS.slaves))
+        print(('Slaves: ' + str(AUTO_REDIS.slaves)))
